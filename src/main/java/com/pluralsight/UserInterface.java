def0f68478e3330a767128e81a587d5b25258ea4
Asteway Nigussie <asteway17@gmail.com>
2025-05-09 14:35:24 -0500

    save point for current info

--- src/main/java/com/pluralsight/UserInterface.java
+++ src/main/java/com/pluralsight/UserInterface.java
@@ -1,4 +1,11 @@
 package com.pluralsight;
 
 public class UserInterface {
+
+    private Dealership dealership;
+
+    public UserInterface() {}
+
+
+
 }
--- src/main/java/com/pluralsight/Vehicle.java
+++ src/main/java/com/pluralsight/Vehicle.java
@@ -1,4 +1,14 @@
 package com.pluralsight;
 
 public class Vehicle {
+
+    private int vin;
+    private int year;
+    private String make;
+    private String model;
+    private String vehicleType;   // (Car, truck, etc..)
+    private String color;
+    private int odometer;
+    private double price;
+
 }

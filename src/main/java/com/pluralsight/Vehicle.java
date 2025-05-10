0b87b320d692ea4887672673a4d936991948fd5c
Asteway Nigussie <asteway17@gmail.com>
2025-05-09 22:26:21 -0500

    added in most of the code for the vehicle class

--- src/main/java/com/pluralsight/Vehicle.java
+++ src/main/java/com/pluralsight/Vehicle.java
@@ -2,6 +2,7 @@ package com.pluralsight;
 
 public class Vehicle {
 
+    //
     private int vin;
     private int year;
     private String make;
@@ -11,8 +12,8 @@ public class Vehicle {
     private int odometer;
     private double price;
 
-    public Vehicle(int vin, int year, String make, String model,
-                   String vehicleType, String color, int odometer, double price) {
+    //
+    public Vehicle(int vin, int year, String make, String model, String vehicleType, String color, int odometer, double price) {
         this.vin = vin;
         this.year = year;
         this.make = make;
@@ -23,67 +24,28 @@ public class Vehicle {
         this.price = price;
     }
 
-    public int getVin() {
-        return vin;
-    }
+    //
+    public int getVin() { return vin; }
+    public void setVin(int vin) { this.vin = vin; }
+    public int getYear() { return year; }
+    public void setYear(int year) { this.year = year; }
+    public String getMake() { return make; }
+    public void setMake(String make) { this.make = make; }
+    public String getModel() { return model; }
+    public void setModel(String model) { this.model = model; }
+    public String getVehicleType() { return vehicleType; }
+    public void setVehicleType(String vehicleType) { this.vehicleType = vehicleType; }
+    public String getColor() { return color; }
+    public void setColor(String color) { this.color = color; }
+    public int getOdometer() { return odometer; }
+    public void setOdometer(int odometer) { this.odometer = odometer; }
+    public double getPrice() { return price; }
+    public void setPrice(double price) { this.price = price; }
 
-    public void setVin(int vin) {
-        this.vin = vin;
-    }
-
-    public int getYear() {
-        return year;
-    }
-
-    public void setYear(int year) {
-        this.year = year;
-    }
-
-    public String getMake() {
-        return make;
-    }
-
-    public void setMake(String make) {
-        this.make = make;
-    }
-
-    public String getModel() {
-        return model;
-    }
-
-    public void setModel(String model) {
-        this.model = model;
-    }
-
-    public String getVehicleType() {
-        return vehicleType;
-    }
-
-    public void setVehicleType(String vehicleType) {
-        this.vehicleType = vehicleType;
-    }
-
-    public String getColor() {
-        return color;
-    }
-
-    public void setColor(String color) {
-        this.color = color;
-    }
-
-    public int getOdometer() {
-        return odometer;
-    }
-
-    public void setOdometer(int odometer) {
-        this.odometer = odometer;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
+    //
+//    @Override
+//    public String toString() {
+//        return String.format("%d | %d | %s | %s | %s | %s | %d | $%.2f",
+//                vin, year, make, model, vehicleType, color, odometer, price);
+//    }
 }
